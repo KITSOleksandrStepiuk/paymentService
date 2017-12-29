eda3bebc9065a6d8a9b0b0074fea0a40e19c2d88
KITSMykolaIevpak <Mykola_Ievpak@epam.com>
2017-12-29 17:19:56 +0200

    [PYMT-20]  Optile session initiation, implement unit tests

--- src/test/java/com/kingfisher/payment/api/controller/PaymentAPITest.java
+++ src/test/java/com/kingfisher/payment/api/controller/PaymentAPITest.java
@@ -5,10 +5,13 @@ import com.kingfisher.payment.api.database.model.CustomerRegistrationInfo;
 import com.kingfisher.payment.api.database.service.CustomerService;
 import com.kingfisher.payment.api.database.service.TransactionLogService;
 import com.kingfisher.payment.api.optile.model.NetworkList;
+import com.kingfisher.payment.api.optile.model.Registration;
 import com.kingfisher.payment.api.optile.model.Transaction;
 import com.kingfisher.payment.api.optile.service.OptileService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +23,11 @@ import org.springframework.util.StreamUtils;
 import java.nio.charset.Charset;
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +52,8 @@ public class PaymentAPITest {
     private CustomerService customerService;
     @MockBean
     private TransactionLogService transactionLogService;
+    @Captor
+    ArgumentCaptor<Transaction> optileServiceTransactionCaptor;
 
     @Test
     public void createPaymentSessionForNewCustomerTest() throws Exception {
@@ -58,7 +67,7 @@ public class PaymentAPITest {
         NetworkList networkList = objectMapper.readValue(output, NetworkList.class);
 
 
-        given(optileService.postListRequest(transaction)).willReturn(networkList);
+        given(optileService.postListRequest(any(Transaction.class))).willReturn(networkList);
 
 
         mvc.perform(post(URL_BASE+"/session/create").contentType(MediaType.APPLICATION_JSON).content(input))
@@ -77,24 +86,56 @@ public class PaymentAPITest {
         customerRegistrationInfo.setOptileCustomerId(OPTILE_CUSTOMER_ID);
         customerRegistrationInfo.setOptileCustomerPassword(OPTILE_PASSWORD);
 
-        given(customerService.getCustomerRegistrationInfo(PROFILE_ID)).willReturn(Optional.of(customerRegistrationInfo));
-
         String input = StreamUtils.copyToString(this.getClass().getClassLoader().getResourceAsStream("expectedJson/optileListRequestInput.json"), Charset.defaultCharset());
         String output = StreamUtils.copyToString(this.getClass().getClassLoader().getResourceAsStream("expectedJson/optileListRequestOutput.json"), Charset.defaultCharset());
 
-        Transaction transaction = objectMapper.readValue(input, Transaction.class);
         NetworkList networkList = objectMapper.readValue(output, NetworkList.class);
 
-
-        given(optileService.postListRequest(transaction)).willReturn(networkList);
-
+        given(customerService.getCustomerRegistrationInfo(anyString())).willReturn(Optional.of(customerRegistrationInfo));
+        given(optileService.postListRequest(any(Transaction.class))).willReturn(networkList);
 
         mvc.perform(post(URL_BASE+"/session/create").contentType(MediaType.APPLICATION_JSON).content(input))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().json(output));
 
+        verify(optileService).postListRequest(optileServiceTransactionCaptor.capture());
+        Registration registration = optileServiceTransactionCaptor.getValue().getCustomer().getRegistration();
+
+        assertEquals(registration.getId(), OPTILE_CUSTOMER_ID);
+        assertEquals(registration.getPassword(), OPTILE_PASSWORD);
 
     }
 
+    @Test
+    public void missingMandatoryFieldsWhileCratePaymentSession() throws Exception {
+
+        /**
+         Transaction:
+             type: object
+             required:
+             - transactionId - generatedBy payment API service
+             - country
+             - callback
+             - customer
+             - payment
+         */
+
+        given(customerService.getCustomerRegistrationInfo(PROFILE_ID)).willReturn(Optional.empty());
+
+        String input = StreamUtils.copyToString(this.getClass().getClassLoader().getResourceAsStream("expectedJson/missingMandatoryFieldsInput.json"), Charset.defaultCharset());
+        String output = StreamUtils.copyToString(this.getClass().getClassLoader().getResourceAsStream("expectedJson/missingMandatoryFieldsOutput.json"), Charset.defaultCharset());
+
+        NetworkList networkList = objectMapper.readValue(output, NetworkList.class);
+
+        given(optileService.postListRequest(any(Transaction.class))).willReturn(networkList);
+
+        mvc.perform(post(URL_BASE+"/session/create").contentType(MediaType.APPLICATION_JSON).content(input))
+                .andExpect(status().is(400))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(output));
+
+
+
+    }
 }
