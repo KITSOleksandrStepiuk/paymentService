2bf0f8c9f0742ef157246631b43f94bdc086f39b
KITSMykolaIevpak <Mykola_Ievpak@epam.com>
2017-12-29 17:19:00 +0200

    [PYMT-20]  Optile session initiation, service configuration changes

--- src/main/java/com/kingfisher/payment/api/config/JacksonConfig.java
+++ src/main/java/com/kingfisher/payment/api/config/JacksonConfig.java
@@ -3,10 +3,13 @@ package com.kingfisher.payment.api.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class JacksonConfig {
@@ -18,21 +21,10 @@ public class JacksonConfig {
 
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-//        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz"));
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz"));
         objectMapper.registerModule(new JodaModule());
-
-//        objectMapper.registerModule(createJodaModule());
-//        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz"));
-
         return objectMapper;
     }
-
-//    private static SimpleModule createJodaModule() {
-//        SimpleModule module = new SimpleModule();
-//        JacksonJodaFormat jjdf = new JacksonJodaFormat(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSz").withZoneUTC());
-//        module.addSerializer(DateTime.class, new DateTimeSerializer(jjdf));
-//        return module;
-//    }
 
 }
