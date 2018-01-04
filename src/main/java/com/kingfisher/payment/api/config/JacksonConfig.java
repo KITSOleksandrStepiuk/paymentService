7c5a1cf9297ca38ff69461207484482e92a5c99f
KITSMykolaIevpak <Mykola_Ievpak@epam.com>
2018-01-04 17:20:58 +0000

    [PYMT-20] Fix timezone offset serialization

--- src/main/java/com/kingfisher/payment/api/config/JacksonConfig.java
+++ src/main/java/com/kingfisher/payment/api/config/JacksonConfig.java
@@ -5,9 +5,16 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.joda.cfg.JacksonJodaDateFormat;
+import com.fasterxml.jackson.datatype.joda.ser.DateTimeSerializer;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.datetime.joda.DateTimeFormatterFactory;
 
 import java.text.SimpleDateFormat;
 
@@ -16,15 +23,31 @@ public class JacksonConfig {
 
     @Primary
     @Bean
-    public ObjectMapper getObjectMapper() {
+    public ObjectMapper getObjectMapper(@Autowired JodaModule module) {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz"));
-        objectMapper.registerModule(new JodaModule());
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        objectMapper.registerModule(module);
         return objectMapper;
+    }
+
+    @Bean
+    public JodaModule getJodaModule() {
+
+        JodaModule module = new JodaModule();
+
+        DateTimeFormatterFactory dff;
+        dff = new DateTimeFormatterFactory();
+        dff.setPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+        JacksonJodaDateFormat format = new JacksonJodaDateFormat(dff.createDateTimeFormatter());
+
+        module.addSerializer(DateTime.class, new DateTimeSerializer(format, 0));
+
+        return module;
     }
 
 }
